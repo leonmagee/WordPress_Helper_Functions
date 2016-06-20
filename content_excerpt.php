3f769ff695dc52946bff57b26c7e72b089483f8c
Leon Magee <leonmagee@hotmail.com>
2016-06-20 12:33:53 -0700

    update

--- content_excerpt.php
+++ content_excerpt.php
@@ -1,13 +1,24 @@
 <?php
-
 /**
  * Content Excerpt
  *
  * @param $content
  * @param $length
  * @param string $suffix
+ *
+ * @return string
  */
 function content_excerpt( $content, $length, $suffix = '...' ) {
 
+	$string = substr( $content, 0, $length );
 
+	$exploded = explode( ' ', $string );
+
+	array_pop( $exploded );
+
+	$implode = implode( ' ', $exploded );
+
+	$final = $implode . $suffix;
+
+	return $final;
 }
